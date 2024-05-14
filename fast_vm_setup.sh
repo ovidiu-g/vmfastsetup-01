0701d7b021aa91ce9aa87d99a4daf3198d36ac70
Ovidiu-Ionuț Gherman <83766960+ovidiu-g@users.noreply.github.com>
2024-05-14 18:58:04 +0300

    Update fast_vm_setup.sh

--- fast_vm_setup.sh
+++ fast_vm_setup.sh
@@ -2,6 +2,6 @@
 
 apt update
 apt upgrade
-apt install gcc make cmake dkms build-essential mc tree nano btop htop atop screen tmux tldr wget curl zip unzip openssh-server openssh-client nala git sqlite3 python3 python3-dev python3-setuptools python3-wheel python3-pip python3-venv firefox
+apt install gcc make cmake dkms build-essential mc tree nano btop htop atop screen tmux tldr wget curl zip unzip openssh-server openssh-client nala git sqlite3 python3 python3-dev python3-setuptools python3-wheel python3-pip python3-venv
 atoremove
 updatedb
